66fd236341cfeb45744dab1d41e542c4a82cace7
Georg Schmidt <gs-develop@gs-sys.de>
2018-11-08 22:00:47 +0100

    Java Upload

---
+++ java/src/HTTP_Upload.java
@@ -0,0 +1,64 @@
+import org.apache.http.HttpStatus;
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class HTTP_Upload {
+    private static String url = "https://<domain>/upload.php?auth=<keyX>";
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // file to upload
+        File sendFile = new File("demo.txt");
+
+        String httpInputName = "userfile";
+
+        HttpClient client = new HttpClient();
+        PostMethod method = new PostMethod(url);
+
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+                new DefaultHttpMethodRetryHandler(3, false));
+
+        // add files
+        Part[] parts = {
+                new FilePart(httpInputName, sendFile)
+        };
+
+        // enctype="multipart/form-data"
+        method.setRequestEntity(
+                new MultipartRequestEntity(parts, method.getParams())
+        );
+
+        try {
+            int statusCode = client.executeMethod(method);
+
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed: " + method.getStatusLine());
+            }
+
+            // Read the response body.
+            byte[] responseBody = method.getResponseBody(40960);
+
+            // May fail if binaries are send by the server!
+            System.out.println(new String(responseBody));
+
+        } catch (HttpException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Connection errors: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            method.releaseConnection();
+        }
+    }
+}
